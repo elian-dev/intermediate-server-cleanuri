589d2ad796676d75d99daa224389421b9e169631
elian-dev <geordymemdoza@gmail.com>
2023-11-16 02:35:01 -0600

    feat: :sparkles: Create server and paths

---
+++ index.js
@@ -0,0 +1,48 @@
+const fetch = require('node-fetch');
+const express = require('express')
+
+const PORT = 3001
+const app = express()
+app.use(express.json());
+
+// Setup headers CORS
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*"); // Acces to any origin
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    next();
+});
+
+app.post("/clean-uri", (req, res) => {
+    const cleanUriUrl = "https://cleanuri.com/api/v1/shorten";
+  
+    fetch(cleanUriUrl, {
+      method: "POST",
+      headers: { 
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(req.body),
+    })
+        .then((response) => response.json())
+        .then((result) => {
+            res.header("Access-Control-Allow-Origin", "*");
+            res.json(result);
+        })
+        .catch((error) => {
+            console.error("Error requesting cleanuri.com:", error);
+            res.status(500).json({ error: "Intermediate server error" });
+        });
+});
+
+
+app.get('/', (req, res) => {
+    res.json(
+        { 
+            name: 'Sever to avoid cors of CleanURI', 
+            author: 'eliandev',
+            url: '/clean-uri',
+            method: 'POST'
+        })
+})
+
+app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`))
